cecad4ffa2828551c9fc4711dc2067a5bdf57768
daimingzhang@gmail.com <daimingzhang@gmail.com@2fbe8073-b774-1b62-5e22-ac94b54f8278>
2011-12-12 12:26:05 +0000

    FaceEncrypt

---
+++ trunk/FaceEncrypt.cpp
@@ -0,0 +1,187 @@
+// FaceEncrypt.cpp : Defines the class behaviors for the application.
+//
+
+#include "stdafx.h"
+#include "FaceEncrypt.h"
+
+#include "./AboutView/MainFrm.h"
+#include "FaceEncryptDoc.h"
+#include "./AboutView/FaceEncryptView.h"
+#include "./AboutView/ChooseOperation.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CFaceEncryptApp
+
+BEGIN_MESSAGE_MAP(CFaceEncryptApp, CWinApp)
+	//{{AFX_MSG_MAP(CFaceEncryptApp)
+	ON_COMMAND(ID_APP_ABOUT, OnAppAbout)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG_MAP
+	// Standard file based document commands
+	ON_COMMAND(ID_FILE_NEW, CWinApp::OnFileNew)
+	ON_COMMAND(ID_FILE_OPEN, CWinApp::OnFileOpen)
+	// Standard print setup command
+	ON_COMMAND(ID_FILE_PRINT_SETUP, CWinApp::OnFilePrintSetup)
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CFaceEncryptApp construction
+
+CFaceEncryptApp::CFaceEncryptApp()
+{
+	// TODO: add construction code here,
+	// Place all significant initialization in InitInstance
+}
+/////////////////////////////////////////////////////////////////////////////
+// The one and only CFaceEncryptApp object
+
+CFaceEncryptApp theApp;
+
+/////////////////////////////////////////////////////////////////////////////
+// CFaceEncryptApp initialization
+
+BOOL CFaceEncryptApp::InitInstance()
+{
+    //AfxMessageBox("InitInstance()");
+	{	// BLOCK: doc template registration
+		// Register the document template.  Document templates serve
+		// as the connection between documents, frame windows and views.
+		// Attach this form to another document or frame window by changing
+		// the document or frame class in the constructor below.
+		CSingleDocTemplate* pNewDocTemplate = new CSingleDocTemplate(
+			IDR_CHOOSEOPERATION_TMPL,
+			RUNTIME_CLASS(CFaceEncryptDoc),		// document class
+			RUNTIME_CLASS(CMainFrame),		// frame class
+			RUNTIME_CLASS(CChooseOperation));		// view class
+		AddDocTemplate(pNewDocTemplate);
+	}
+
+	AfxEnableControlContainer();
+
+	// Standard initialization
+	// If you are not using these features and wish to reduce the size
+	//  of your final executable, you should remove from the following
+	//  the specific initialization routines you do not need.
+
+#ifdef _AFXDLL
+	Enable3dControls();			// Call this when using MFC in a shared DLL
+#else
+	Enable3dControlsStatic();	// Call this when linking to MFC statically
+#endif
+
+	// Change the registry key under which our settings are stored.
+	// TODO: You should modify this string to be something appropriate
+	// such as the name of your company or organization.
+	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+
+	LoadStdProfileSettings();  // Load standard INI file options (including MRU)
+
+	// Register the application's document templates.  Document templates
+	//  serve as the connection between documents, frame windows and views.
+
+	CSingleDocTemplate* pDocTemplate;
+	pDocTemplate = new CSingleDocTemplate(
+		IDR_MAINFRAME,
+		RUNTIME_CLASS(CFaceEncryptDoc),
+		RUNTIME_CLASS(CMainFrame),       // main SDI frame window
+		RUNTIME_CLASS(CFaceEncryptView));
+	AddDocTemplate(pDocTemplate);
+
+	// Enable DDE Execute open
+	EnableShellOpen();
+	RegisterShellFileTypes(TRUE);
+
+	// Parse command line for standard shell commands, DDE, file open
+	CCommandLineInfo cmdInfo;
+	ParseCommandLine(cmdInfo);
+
+	// Dispatch commands specified on the command line
+	if (!ProcessShellCommand(cmdInfo))
+		return FALSE;
+
+	// The one and only window has been initialized, so show and update it.
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+
+	// Enable drag/drop open
+	m_pMainWnd->DragAcceptFiles();
+	return TRUE;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	//{{AFX_DATA(CAboutDlg)
+	enum { IDD = IDD_ABOUTBOX };
+	//}}AFX_DATA
+
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAboutDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	//{{AFX_MSG(CAboutDlg)
+	virtual void OnOK();
+	afx_msg void OnAppExit();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+	//{{AFX_DATA_INIT(CAboutDlg)
+	//}}AFX_DATA_INIT
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CAboutDlg)
+	//}}AFX_DATA_MAP
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+	//{{AFX_MSG_MAP(CAboutDlg)
+	ON_COMMAND(ID_APP_EXIT, OnAppExit)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+// App command to run the dialog
+void CFaceEncryptApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CFaceEncryptApp message handlers
+
+
+void CAboutDlg::OnOK() 
+{
+	// TODO: Add extra validation here
+	
+	CDialog::OnOK();
+}
+
+void CAboutDlg::OnAppExit() 
+{
+	// TODO: Add your command handler code here
+}
