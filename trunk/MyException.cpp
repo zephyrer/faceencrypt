556e4904e5250d0afa13516f3b225cd398099b9d
daimingzhang@gmail.com <daimingzhang@gmail.com@2fbe8073-b774-1b62-5e22-ac94b54f8278>
2011-12-12 12:27:31 +0000

    异常处理

---
+++ trunk/MyException.cpp
@@ -0,0 +1,27 @@
+// MyException.cpp: implementation of the CMyException class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "FaceEncrypt.h"
+#include "MyException.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CMyException::CMyException()
+{
+
+}
+
+CMyException::~CMyException()
+{
+
+}
